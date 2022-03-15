f85fc47ec39f4145b4b4f62f2b1974e69f49f4f8
Vinicius Trajano <vinicius.trajano@linkapi.com.br>
2022-03-15 12:05:43 -0300

    adding information

--- Python/variaveis.py
+++ Python/variaveis.py
@@ -1,3 +1,10 @@
 # Declaração de variáveis, não precisa especificar tipos:
 resultado = "Super" + "Mario"
 print(resultado)
+
+nome = "Marcos"
+print(nome)
+
+a = "anos"
+idade = "20"
+print(idade + a)
