5d0c06733de7b3b7f1714e1fd7114ec9fdc45d3b
Vinicius Trajano <vinicius.trajano@linkapi.com.br>
2022-03-15 11:22:50 -0300

    Starting recap on python classes

---
+++ AulaPython/aulaRecaptulação.py
@@ -0,0 +1,13 @@
+# comando print:
+print("Hello World")
+
+# Operações matemáticas python:
+print(2 + 3)
+print(2**3)  # potencia
+print(5 / 2)  # Divisão real
+print(5 // 2)  # divisão inteira
+print(7 % 3)  # mod resto de divisão
+
+# Declaração de variáveis, não precisa especificar tipos:
+resultado = "Super" + "Mario"
+print(resultado)
