29f811847fbe2e8d4328b9c69e78058ea0ef39c9
Vinicius Trajano <vinicius.trajano@linkapi.com.br>
2022-03-15 14:40:41 -0300

    adding information

---
+++ Python/comandos_desvios.py
@@ -0,0 +1,24 @@
+# Comando IF:
+
+cargo = input('Insira seu cargo: ')
+
+# O bloco de código em python, não utiliza chaves, é tudo pela identação.
+# A identação no python é extremamante importante, sem ela, além de feio o código,
+# estará errado.
+# Se quiser inserir os () não tem problema.
+if cargo == cargo:
+    print('Acesso liberado')
+else:
+    print('Acesso Negado')
+print('Final')
+
+# caso tenha um cenário no qual o if será um pouco maior, poderá ser feito assim:
+idade = int(input('Insira sua idade: '))
+if idade > 65:
+    print('Acesso prioritário')
+elif idade < 18:
+    print('Acesso negado')
+else:
+    print('Acesso Liberado')
+print('Final')
+# utiliza-se elif, ao invés de else if, para fazer o que chamamos de if aninhado.
